7b2db632bb861c2aebd05f3b93bc553e7aabbc5f
Numkn644 <numkn644@gmail.com>
2015-11-15 11:38:36 +0900

    Main.cpp is changed.

--- soureces/Main.cpp
+++ soureces/Main.cpp
@@ -1,11 +1,28 @@
 #include "DxLib.h"
+#include "Utility\KeyboardInput.h"
+#include "Object\Ball\Normal_Ball.h"
+
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
 	ChangeWindowMode(TRUE);
 	DxLib_Init();
+	SetDrawScreen(DX_SCREEN_BACK);
 
-	WaitKey();
+	Normal_Ball normalBall(3,4);
+
+	normalBall.initialize();
+
+	while (!ProcessMessage() && !ScreenFlip() && !ClearDrawScreen())
+	{
+		KeyboardInput::Instance()->update();
+		if (KeyboardInput::Instance()->get(KEY_INPUT_ESCAPE) == 1){ break; }
+
+		normalBall.update();
+		normalBall.draw();
+	}
+
+	normalBall.finalize();
 
 	DxLib_End();
 
